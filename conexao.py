65e4dd10005a559a8537e3e35492f52badd8054e
Gabriel Carvalho <gabriel.figueiredo.carvalho@gmail.com>
2023-10-05 11:44:37 -0300

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -1,18 +1,11 @@
 import pyodbc
+import os
 
-server_name ='DESKTOP-ADV3BR6'
-database_name = 'Brasileirao'
-username ='sa'
-password ='bAran321!'
+server_name = os.environ.get('SQL_SERVER_NAME')
+database_name = os.environ.get('SQL_DATABASE_NAME')
+username = os.environ.get('SQL_USERNAME')
+password = os.environ.get('SQL_PASSWORD')
 
 conn = pyodbc.connect(
     f'DRIVER=SQL Server;SERVER={server_name};DATABASE={database_name};UID={username};PWD={password}'
 )
-"""
-cursor = conn.cursor()
-cursor.execute("SELECT * from artilharia")
-resultado = cursor.fetchone()
-print(resultado)
-cursor.close()
-conn.close()
-"""
